161c7353def672caf3b9e0544bac9c039b9c28f5
Sagnik Bhattacharya <sagnikbhattacharya1506@gmail.com>
2025-07-03 20:57:16 +0530

    Update Android Gradle to Kotlin DSL
    - Migrated Android build scripts from Groovy to Kotlin DSL.
    - Updated Gradle version to 8.13.
    - Adjusted JVM arguments for Gradle in `gradle.properties`.
    - Updated `flutter_lints` to `^6.0.0`.
    - Modified `.github/workflows/main.yml` to separate build jobs for Windows, Android, Web, and iOS.
    - Updated Flutter and Java versions in CI workflow.
    - Made minor changes to `AndroidManifest.xml` and `.gitignore`.

--- android/app/src/main/kotlin/com/codingliquids/launch_pad/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.codingliquids.launch_pad
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/codingliquids/launch_pad_starting/MainActivity.kt
+++ android/app/src/main/kotlin/com/codingliquids/launch_pad_starting/MainActivity.kt
@@ -2,4 +2,4 @@ package com.codingliquids.launch_pad_starting
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
